4166c81aa9df53126e3ae23db1a73ca3c3414665
Dimitri Bosch <dimitribosch@hotmail.com>
2018-03-17 14:13:29 +0100

    Setting up build before deploy

--- config.rb
+++ config.rb
@@ -49,6 +49,7 @@ page '/*.txt', layout: false
 
 activate :deploy do |deploy|
   deploy.method = :git
+  deploy.build_before = true # default: false
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
